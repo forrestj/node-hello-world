2651885070069d38e62c7632bcf1665d4038bc53
forrestj <forrestj@users.noreply.github.com>
2015-09-23 18:04:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('foo2 - Hello World !');
+    res.send('<html><body>foo2 - Hello World !</body></html>');
 });
 
 app.listen(80);
